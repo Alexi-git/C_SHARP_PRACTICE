c8660581b5f4a8c7f1bb1c61d1a7c72b0b4bda9c
Alexi <kukushkin-94@rambler.ru>
2023-03-01 00:46:41 +0300

    Написал код задачи 2

--- Homework/Homework_01/Ex2/Program.cs
+++ Homework/Homework_01/Ex2/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int max,min;
+
+Console.Write("Введите значение a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+max = 0;
+min = 0;
+
+if (a>b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
