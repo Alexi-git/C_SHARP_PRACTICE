fe4df726ad58e36e629695cdfd732aaf524aff53
Alexi <kukushkin-94@rambler.ru>
2023-03-01 12:23:02 +0300

    Исправил README2

---
+++ Homework/Homework_01/Ex8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int even = 2;
+
+if(number > 1)
+{
+    while(even <= number)
+    {
+        Console.Write(even + " ");
+        even = even + 2;
+    }
+}
